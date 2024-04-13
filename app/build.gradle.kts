46e47d95847482ffad73e32c76ac49e09549d96a
Ramiz Raja <informramiz@gmail.com>
2024-04-14 03:03:43 +0500

    chore: Add Web3j and viewmodel libries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,11 +54,18 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //Web3j
+    implementation(libs.web3j.client)
+
+    // Test and Debug Implementations
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
